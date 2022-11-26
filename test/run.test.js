b8db5b85879e846b84ff27661e9c8bed99e5b70c
Matthias Giger <i@matthiasgiger.com>
2022-11-26 12:53:03 +0100

    build(dist): ensure all files are committed

--- test/run.test.js
+++ test/run.test.js
@@ -2,9 +2,10 @@ import { join } from 'path'
 import { execSync } from 'child_process'
 
 test('Release request in debug mode works without issues.', () => {
-  process.env.INPUT_NPM_TOKEN = 'debug'
+  process.env.INPUT_NPM_TOKEN = 'npm_debug'
   process.env.INPUT_DRY_RUN = 'true'
   process.env.INPUT_DEBUG = 'true'
+  process.env.INPUT_GITHUB_TOKEN = 'something'
 
   try {
     execSync(`node ${join(process.cwd(), 'index.js')}`, { env: process.env }).toString()
