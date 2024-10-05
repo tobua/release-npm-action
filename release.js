9c92b8e1f607167b835772f44dbc2018d1eb8a09
Matthias Giger <i@matthiasgiger.com>
2024-10-05 13:08:47 +0200

    fix(action): commit with proper user

--- release.js
+++ release.js
@@ -121,9 +121,7 @@ export const createRelease = async () => {
           setFailed('Failed to create or publish release.')
         }
         info('There are no relevant changes, so no new version is released.')
-        info(
-          'See https://github.com/tobua/release-npm-action#troubleshooting for more information.',
-        )
+        info('See https://github.com/tobua/release-npm-action#troubleshooting for more information.')
         return
       }
 
